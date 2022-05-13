8e3015488498d7b967f9dcbfe4fe3a7398f48ae7
shulder2@gmail.com <shulder2@gmail.com>
2022-05-13 10:10:35 -0500

    Proyecto Base

--- tienda_online/settings.py
+++ tienda_online/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+#import django_heroku
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
